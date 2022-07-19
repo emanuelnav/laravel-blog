fe87a55afbf2829a5a652f4e215e2a27e3ab3232
Emanuel Navarro <emanuelnavarro.dev@gmail.com>
2022-07-19 14:31:40 -0300

    page controller and routes to see the posts added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+
+Route::get('/post/{post:slug}', [PageController::class, 'post'])->name('post');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

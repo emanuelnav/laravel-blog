1016643e656b8f87fb53b78b39993b262697d73f
Emanuel Navarro <emanuelnavarro.dev@gmail.com>
2022-07-22 16:30:44 -0300

    relations between post and user model added and pagecontroller functions created

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
     public function posts()
     {
         return view('posts', [
-            'posts' => Post::with('user')
+            'posts' => Post::with('user')->latest()->paginate()
         ]);
     }
 
